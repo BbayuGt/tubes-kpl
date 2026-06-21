9ed7af803e39fbfd324d70314b41210dda7d5714
dhai <afrizalganz88@contoh.com>
2026-06-21 18:17:10 +0700

    Nambah Models, Services, Dll

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using tubes_kpl.Components;
+using tubes_kpl.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,21 +7,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+var backendUrl = builder.Configuration["BackendUrl"]
+    ?? "http://localhost:8080/";
+
+builder.Services.AddHttpClient("BackendAPI", client =>
+{
+    client.BaseAddress = new Uri(backendUrl);
+});
+
+// Register Services
+builder.Services.AddScoped<CampaignService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
+app.UseStatusCodePagesWithReExecute(
+    "/not-found",
+    createScopeForStatusCodePages: true);
+
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
